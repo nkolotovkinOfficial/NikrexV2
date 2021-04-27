908a1fde467be5a3d2b8352efbf1f547710c1d66
Nik <72085666+nkolotovkinOfficial@users.noreply.github.com>
2021-04-27 16:47:40 +0300

    Add files via upload

---
+++ tools/addons/logo.py
@@ -0,0 +1,14 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+  _   _ _ _    _____           
+ | \ | (_) |  |  __ \          
+ |  \| |_| | _| |__) |_____  __
+ | . ` | | |/ /  _  // _ \ \/ /
+ | |\  | |   <| | \ \  __/>  < 
+ |_| \_|_|_|\_\_|  \_\___/_/\_\
+                              Created by Nkolotovkin:D
+                               
+{Fore.RESET}""")
